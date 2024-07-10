a556235e53bd4405d57abdb3e031be78d6a3d2c0
Bashir Umar Idris <bashtrust15@gmail.com>
2024-07-10 23:25:50 +0100

    Add some functions

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-const errorField = document.getElementById("error");
 const emailInput = document.getElementById("email");
+const errorMsg = document.getElementById("error-msg");
+const emailValue = document.getElementById("email").value;
 
-function addError() {
-    emailInput.placeholder = "no value";
-    errorField.innerText = "this isn't a valid email"
-    emailInput.style.border = "1px solid red";
-    errorField.style.color = "red";
+
+function addErrorMsg() {
+    // errorMsg.innerText = "Please enter your email!";
+    errorMsg.style.color = "#df1a1a";
+    emailInput.style.border = "1px solid #df1a1a";
 }
+
 function submitForm(e) {
-    console.log(e, "e");
     e.preventDefault();
 
-const emailInput = document.getElementById("email");
-const emailValue = emailInput.value;
+    const formElement = emailInput.value;
+    const validInput = formElement.checkValidity();
 
-
-    const form = document.getElementById("sub-form");
-    // const valid = form.checkValidity;
-    // console.log("valid: ", valid);
-    console.log(emailValue, emailValue == "", "boolean email");
-
-
-
-    if (emailValue == "") {
-        addError()
-    } 
-    // else {
-       
-    // }
+    if (validInput) {
+        window.location.href("./successMsg.html")
+    } else {
+        if (emailValue === "") {
+            addErrorMsg();
+            errorMsg.innerText = "Email field must be filled!";
+        } else {
+            addErrorMsg();
+            errorMsg.innerText = "Please enter a valid email!";
+        }
+    }
 }
 
-emailInput.addEventListener("input", ()=>{
-    const emailInput = document.getElementById("email");
-    const emailValue = emailInput.value;
-
-    if (!Boolean(emailValue)) {
-        addError()
-    } else {
-        console.log("I have been changed");
-        errorField.innerText = ""
-    }
-
-
-})
-
-
+emailInput.addEventListener("input", submitForm);
