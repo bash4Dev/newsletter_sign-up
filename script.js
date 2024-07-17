779aaa50f645b45149c316bb4519b8003d63553f
Bashir Umar Idris <bashtrust15@gmail.com>
2024-07-17 15:52:04 +0100

    Modify the script file

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ function addErrorMsg(msg) {
     errorMsg.style.display = "block";
     errorMsg.style.color = "#df1a1a"
     emailInput.style.border = "1px solid #df1a1a";
-    emailInput.classList.add("invalid");
     errorMsg.innerText = msg;
 }
 
 function clearErrorMsg() {
     errorMsg.style.display = "none";
-    emailInput.classList.remove("invalid");
     errorMsg.innerText = "";
 }
 
@@ -32,9 +30,13 @@ function submitForm(e) {
     } else if (!validateEmail(emailValue)) {
         addErrorMsg("Please enter a valid email!");
     } else {
-        return window.location.href = "./successMsg.html";
+        return window.location.href = "./success-msg.html";
     }
 }
 
+// const dismissBtn = document.getElementById("dismiss-btn");
+// dismissBtn.addEventListener("click", () => {
+//     history.back();
+// });
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", submitForm);
